1a38da33aa37b2fb2d67789b642b3c3723dc14e6
Aguinaldo <guiga_oli@hotmail.com>
2023-03-07 17:15:38 -0300

    second

--- assets/scripts/mainAnimation.js
+++ assets/scripts/mainAnimation.js
@@ -1,6 +1,23 @@
+// função para quando o mouse entra em uma carta
+function handleMouseEnter() {
+    this.classList.add('sCard--hovered');
+    document.body.id = `${this.id}-hovered`;
+}
+// função para quando o mouse sai de uma carta
+function handleMouseLeave() {
+    this.classList.remove('sCard--hovered');
+    document.body.id = '';
+}
+
+// função para adicionar eventos ao mouse
 function addEventListenersToCards() {
-    const cardElements = document.getElementById("sCard");
-    console.log(cardElements);
+    const cardElements = document.getElementsByClassName("sCard");
+    
+    for(let index = 0; index <cardElements.length; index++){
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
