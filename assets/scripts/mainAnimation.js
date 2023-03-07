9fab781da736f644c0bd70ffabf9ede6883d2c16
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-03-07 10:35:28 -0300

    second

---
+++ assets/scripts/mainAnimation.js
@@ -0,0 +1,6 @@
+function addEventListenersToCards() {
+    const cardElements = document.getElementById("sCard");
+    console.log(cardElements);
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
